1d7139c5e07a8df0c7b62880fbdad868a816c80d
Scott Albertson <scott@thoughtbot.com>
2014-09-24 15:01:50 -0700

    Make JavaScript style guide configurable

--- app/models/style_guide/java_script.rb
+++ app/models/style_guide/java_script.rb
@@ -3,9 +3,19 @@ module StyleGuide
     DEFAULT_CONFIG_FILE = File.join(CONFIG_DIR, "javascript.json")
 
     def violations_in_file(file)
-      Jshintrb.lint(file.content, :defaults).map do |violation|
+      Jshintrb.lint(file.content, config).map do |violation|
         Violation.new(file, violation["line"], violation["reason"])
       end
+    end
+
+    private
+
+    def config
+      default_config.merge(repo_config.for(name))
+    end
+
+    def default_config
+      JSON.parse(File.read(DEFAULT_CONFIG_FILE))
     end
   end
 end
--- spec/models/style_guide/java_script_spec.rb
+++ spec/models/style_guide/java_script_spec.rb
@@ -1,22 +1,39 @@
 require "fast_spec_helper"
 require "jshintrb"
+require "active_support/inflector"
 require "app/models/violation"
 require "app/models/style_guide/base"
 require "app/models/style_guide/java_script"
 
 describe StyleGuide::JavaScript do
   describe "#violations_in_file" do
-    context "when semicolon is missing" do
-      it "returns violation" do
-        style_guide = StyleGuide::JavaScript.new(double("RepoConfig"))
-        file = double(:file, content: "var blahh = 'blahh'")
-        violation = double("Violation")
-        Violation.stub(new: violation)
+    context "with default config" do
+      context "when semicolon is missing" do
+        it "returns violation" do
+          style_guide = StyleGuide::JavaScript.new(double("RepoConfig", for: {}))
+          file = double(:file, content: "var blahh = 'blahh'")
+          violation = double("Violation")
+          Violation.stub(new: violation)
 
-        violations = style_guide.violations_in_file(file)
+          violations = style_guide.violations_in_file(file)
 
-        expect(Violation).to have_received(:new).with(file, 1, "Missing semicolon.")
-        expect(violations.first).to eq violation
+          expect(Violation).to have_received(:new).with(file, 1, "Missing semicolon.")
+          expect(violations.first).to eq violation
+        end
+      end
+    end
+
+    context "when curly brace rule is disabled using custom config" do
+      context "with missing curly braces" do
+        it "returns no violation" do
+          custom_config = double("RepoConfig", for: { "curly" => false })
+          style_guide = StyleGuide::JavaScript.new(custom_config)
+          file = double(:file, content: "while(true) var test = 'test';")
+
+          violations = style_guide.violations_in_file(file)
+
+          expect(violations).to be_empty
+        end
       end
     end
   end
